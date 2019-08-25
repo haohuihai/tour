915d27b904f57f49905472e0a88e482a054d54c1
haohuihai <157855644@qq.com>
2019-08-25 08:44:13 +0800

    init

---
+++ src/page/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+  import homeHeader from './components/homeHeader'
+  export default {
+    name: 'Home',
+components:{
+  homeHeader,
+}
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/page/home/components/homeHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">homeHeader</div>
+</template>
+
+<script>
+  export default {
+    name: 'homeHeader',
+
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+.test
+  color: red
+</style>
